62153bb25e3867217d230b2e093655e942965b3d
conradg-s <conradguo99@gmail.com>
2025-03-21 12:32:53 +1000

    Update T5-pdc-mods.sql

--- T5-pdc-mods.sql
+++ T5-pdc-mods.sql
@@ -1,17 +1,3 @@
---*****PLEASE ENTER YOUR DETAILS BELOW*****
---T5-pdc-mods.sql
-
--- ITO Assignment 2 Task 5
-
---Student ID: 35484306
---Student Name: Conrad Guo
-
-/* Comments for your marker:
-
-
-
-
-*/
 
 -- ENSURE that your SQL code is formatted and has a semicolon (;)
 -- at the end of every statement. When marked this will be run as
